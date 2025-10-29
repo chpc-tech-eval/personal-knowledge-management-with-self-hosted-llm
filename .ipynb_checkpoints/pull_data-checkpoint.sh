6986e571f1b6ec6e6a60263a6a402a82859d6c15
Shaun Beautement <sf.beautement@protonmail.com>
2025-10-29 16:39:51 +0000

    implement github action to generate vector DB

--- .ipynb_checkpoints/pull_data-checkpoint.sh
+++ .ipynb_checkpoints/pull_data-checkpoint.sh
@@ -6,8 +6,8 @@ set -euo pipefail
 #
 REPO_DIR=$(dirname "$0")
 DATA_URL="https://github.com/chpc-tech-eval/scc"
-OUTPUT_DIR="$REPO_DIR/data/raw"
-TEMP_GITHUB="/tmp/selection_round_github"
+OUTPUT_DIR="/opt/shared/data/raw"
+TEMP_GITHUB="/opt/shared/data/_selection_round_github"
 
 
 mkdir -p "$OUTPUT_DIR"
